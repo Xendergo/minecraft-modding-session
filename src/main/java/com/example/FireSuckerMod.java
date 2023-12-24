1284a6313cdea44ac3317d3787f825ea98256a0e
Henry Rovnyak <h.rovnyak@proton.me>
2023-12-24 15:01:25 -0500

    Make the base to build on top of for the session

--- src/main/java/com/example/FireSuckerMod.java
+++ src/main/java/com/example/FireSuckerMod.java
@@ -1,14 +1,6 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,19 +10,12 @@ public class FireSuckerMod implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("firesucker");
 
-    public static final Block FIRE_SUCKER = new FireSucker();
-
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
 
-        Registry.register(Registries.BLOCK, new Identifier("firesucker", "firesucker"), FIRE_SUCKER);
-        var item = new BlockItem(FIRE_SUCKER, new FabricItemSettings());
-        Registry.register(Registries.ITEM, new Identifier("firesucker", "firesucker"), item);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(content -> {
-            content.add(item);
-        });
+        LOGGER.info("Hello Fabric world!");
     }
 }
